5bea32db8d4f44d2f426881722329babf8564a53
amribrahim <amr.ibrahem.shaker@gmail.com>
2018-06-10 15:24:41 -0500

    Amr -- Lab10

---
+++ Lab10/Problem03/person.js
@@ -0,0 +1,19 @@
+var Person = /** @class */ (function () {
+    function Person() {
+        this._firstname = "";
+    }
+    Object.defineProperty(Person.prototype, "firstname", {
+        get: function () {
+            return this._firstname;
+        },
+        set: function (value) {
+            this._firstname = value.toUpperCase();
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Person;
+}());
+var person = new Person();
+person.firstname = "Asaad";
+console.log(person.firstname);
